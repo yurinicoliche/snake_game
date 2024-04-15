a3a502ddb505971c187c13b2433c81f63d82226e
unknown <yurinicoliche2003@gmail.com>
2024-04-15 14:49:02 -0300

    changes

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const finalScore = document.querySelector(".final-score > span")
 const menu = document.querySelector(".menu-screen")
 const buttonPlay = document.querySelector(".btn-play")
 
-const audio = new Audio("../assets/audio.mp3")
+const audio = new Audio("/assets/audio.mp3")
 
 const size = 30
 
